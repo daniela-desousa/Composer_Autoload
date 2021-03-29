be32b415345d5c974b9700370273aebb60d27983
daniela-desousa <75320196+daniela-desousa@users.noreply.github.com>
2021-03-29 14:01:59 +0200

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'daniela/quetec_composer',
+  ),
+  'versions' => 
+  array (
+    'daniela/quetec_composer' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
